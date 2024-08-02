817ec7c590abce454d31934736838fa949007547
Jack <flaxes09@gmail.com>
2024-08-02 19:19:42 +0300

    Hotfix #1

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ const app = require("./core/server");
 const apiRouter = require("./router");
 const updater = require("./core/updater");
 
+const DEBUG = ["1", "true", "y"].includes(process.env.DEBUG || "");
+
 async function main() {
-    const version = updater.getVersion();
-    if (version.version !== version.available) {
+    const isNewVersionAvailable = await updater.checkAvailable();
+
+    if (isNewVersionAvailable) {
+        console.log("NEW VERSION AVAILABLE! Downloading...");
+
         await updater.update();
 
-        console.log("UPDATED! Please restart program.");
+        console.log("UPDATED! Please restart program. Auto-exit in 5 seconds...");
 
         setTimeout(() => {
             process.exit(0);
--- test.js
+++
@@ -1,8 +0,0 @@
-const moment = require("moment-timezone");
-const week = "2024-W23";
-const DATE_FORMAT = "DD-MM-yyyy";
-
-
-const d = moment(week, 'YYYY-[W]WW');
-
-console.log(d.format(DATE_FORMAT));
