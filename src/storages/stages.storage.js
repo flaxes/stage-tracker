c9cd97e99690faf26e73270cebbb4df82e21dded
Jack <flaxes09@gmail.com>
2024-08-02 19:26:32 +0300

    Updated default stages

--- src/storages/stages.storage.js
+++ src/storages/stages.storage.js
@@ -3,29 +3,29 @@ const Store = require("../core/store");
 const stageDefaults = {
     data: {
         1: {
-            name: "Research",
+            name: "RES",
             id: 1,
-            createdAt: 1717020000000,
+            createdAt: 1722436316646,
         },
         2: {
             name: "BnW",
             id: 2,
-            createdAt: 1717020000000,
+            createdAt: 1717024854481,
         },
         3: {
             name: "Color",
             id: 3,
-            createdAt: 1717020000000,
+            createdAt: 1717024853409,
         },
         4: {
             name: "Render",
             id: 4,
-            createdAt: 1717020000000,
+            createdAt: 1717024855270,
         },
         5: {
-            name: "PSD_prep",
+            name: "PSD",
             id: 5,
-            createdAt: 1717020000000,
+            createdAt: 1722436329811,
         },
     },
     lastIndex: 5,
